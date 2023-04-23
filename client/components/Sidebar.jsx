d18d8acaba6e209a4e6db926d7175dbd3752ee7f
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-04-23 18:58:21 -0400

    update sidebar

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react"
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const Sidebar = ({token}) => {
+const Sidebar = ({ token }) => {
   const router = useRouter();
   const navLinks = [
     {
@@ -21,30 +21,31 @@ const Sidebar = ({token}) => {
 
   const [active, setActive] = useState("/");
   return (
-    <div className='bg-black h-[100vh] w-[250px] fixed flex flex-col justify-start items-center shadow-2xl z-50'>
-      <h3 className="text-white font-bold text-[28px] pt-5">Spot my <span className="text-spotify">Stats</span></h3>
+    <div className="bg-black h-[100vh] w-[250px] fixed flex flex-col justify-start items-center shadow-2xl z-50">
+      <h3 className="text-white font-bold text-[28px] pt-5">
+        Spot my <span className="text-spotify">Stats</span>
+      </h3>
       <div className="flex flex-col justify-start items-start w-full pl-6">
         <p className="text-semiwhite text-[22px]">Menu</p>
         {navLinks.map((link, index) => (
           <div
             key={index}
-            className={`flex flex-row justify-start items-center w-full py-2 px-4 cursor-pointer ${
-              active === link.path ? "border-r-[4px] border-spotify" : ""
+            className={`duration-150 ease-in transform border-r-[4px] flex flex-row border-spotify justify-start items-center w-full py-2 px-4 cursor-pointer ${
+              active === link.path ? "border-opacity-100 " : "border-opacity-0 hover:border-opacity-100"
             }`}
             onClick={() => {
               setActive(link.path);
               router.push(link.path);
             }}
           >
-            
             <Link href={link.path}>
-                <p className="text-semiwhite">{link.name}</p>
+              <p className="text-semiwhite">{link.name}</p>
             </Link>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- client/pages/topTrack.js
+++ client/pages/topTrack.js
@@ -27,10 +27,10 @@ const TopTrack = () => {
           rel="stylesheet"
         />
       </Head>
-      <div>
+      <section className={` w-full min-h-[100vh] overflow-hidden bg-[#121212] xl:pl-[370px] xl:pr-[120px] lg:pl-[350px] lg:pr-[100px] md:pl-36 md:pr-[80px] pl-6 pr-6 py-24 z-[5] flex flex-col justify-start items-start `}>
         <h1>TopTrack</h1>
         <p>Token: {token}</p>
-      </div>
+      </section >
     </>
   );
 };
